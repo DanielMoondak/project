a0f7cc822f4c7730d8eb2bb2225c215f233d3f35
DanielMoondak <danielruperto26@gmail.com>
2024-06-22 01:25:32 -0600

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, jsonify, send_from_directory, render_template
 from flask_mysqldb import MySQL
-import pandas as pd
 import os
 
 app = Flask(__name__)
 
 # Configuración de la base de datos MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'id22349349_daniel'
-app.config['MYSQL_PASSWORD'] = 'Dani123$'
-app.config['MYSQL_DB'] = 'id22349349_dashboard'
+app.config['MYSQL_HOST'] = 'localhost'  # Actualiza con tu host MySQL
+app.config['MYSQL_USER'] = 'id22349349_daniel'  # Actualiza con tu usuario MySQL
+app.config['MYSQL_PASSWORD'] = 'Dani123$'  # Actualiza con tu contraseña MySQL
+app.config['MYSQL_DB'] = 'id22349349_dashboard'  # Actualiza con tu base de datos MySQL
 
 # Inicialización de la extensión MySQL
 mysql = MySQL(app)
@@ -24,14 +23,17 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
-# Ruta para obtener datos del archivo CSV como JSON
+# Ruta para obtener datos desde la base de datos MySQL y devolverlos como JSON
 @app.route('/data')
 def get_data():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM wifi")
-    data = cur.fetchall()
-    cur.close()
-    return jsonify(data)
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM wifi")
+        data = cur.fetchall()
+        cur.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Devuelve un error 500 en caso de excepción
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
