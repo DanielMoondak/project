7e7f4b57b2aa177be3fff5f8cc357ca29d9dba71
DanielMoondak <danielruperto26@gmail.com>
2024-06-22 01:18:36 -0600

    app.py

--- app.py
+++ app.py
@@ -1,44 +1,38 @@
-from flask import Flask, jsonify, render_template
-from flaskext.mysql import MySQL
+from flask import Flask, jsonify, send_from_directory, render_template
+from flask_mysqldb import MySQL
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-# Configuración de la conexión a MySQL
-app.config['MYSQL_DATABASE_HOST'] = 'danielvila.000webhostapp.com'
-app.config['MYSQL_DATABASE_PORT'] = 3306  # Puerto por defecto para MySQL
-app.config['MYSQL_DATABASE_USER'] = 'id22349349_daniel'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Dani123$'
-app.config['MYSQL_DATABASE_DB'] = 'id22349349_dashboard'
+# Configuración de la base de datos MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'id22349349_daniel'
+app.config['MYSQL_PASSWORD'] = 'Dani123$'
+app.config['MYSQL_DB'] = 'id22349349_dashboard'
 
-mysql = MySQL()
-mysql.init_app(app)
+# Inicialización de la extensión MySQL
+mysql = MySQL(app)
 
 # Ruta para servir el archivo index.html desde la carpeta static
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('static', 'index.html')
 
 # Ruta para servir el dashboard.html desde la carpeta templates
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-# Ruta para obtener datos desde la base de datos MySQL
+# Ruta para obtener datos del archivo CSV como JSON
 @app.route('/data')
 def get_data():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-
-    # Ejemplo de consulta SQL, ajusta según tu estructura de tabla
-    query = "SELECT Nombre, Puntos_de_acceso, Costo_punto FROM wifi"
-    cursor.execute(query)
-    data = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    data_dict = [{'Nombre': row[0], 'Puntos_de_acceso': row[1], 'Costo_punto': row[2]} for row in data]
-    return jsonify(data_dict)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM wifi")
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
